c3bdde85b92c5326b44c65554f88bb14eff73212
Gathuku Samwel <gathsam3446@gmail.com>
2024-07-01 23:14:35 +0300

    Testing database

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -9,15 +9,20 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
-# Configure the database connection here
-db.init_app(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
 
-@app.route('/restaurants', methods=['GET'])
+migrate = Migrate(app, db)
+db.init_app(app)
+api = Api(app)
+
+@app.route("/restaurants", methods=['GET'])
 def get_restaurants():
     restaurants = Restaurant.query.all()
     return jsonify([restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants])
 
-@app.route('/restaurants/<int:id>', methods=['GET'])
+@app.route("/restaurants/<int:id>", methods=['GET'])
 def get_restaurant(id):
     restaurant = Restaurant.query.get(id)
     if restaurant:
@@ -25,7 +30,7 @@ def get_restaurant(id):
     else:
         return jsonify({"error": "Restaurant not found"}), 404
 
-@app.route('/restaurants/<int:id>', methods=['DELETE'])
+@app.route("/restaurants/<int:id>", methods=['DELETE'])
 def delete_restaurant(id):
     restaurant = Restaurant.query.get(id)
     if restaurant:
@@ -35,12 +40,12 @@ def delete_restaurant(id):
     else:
         return jsonify({"error": "Restaurant not found"}), 404
 
-@app.route('/pizzas', methods=['GET'])
+@app.route("/pizzas", methods=['GET'])
 def get_pizzas():
     pizzas = Pizza.query.all()
     return jsonify([pizza.to_dict(only=('id', 'name', 'ingredients')) for pizza in pizzas])
 
-@app.route('/restaurant_pizzas', methods=['POST'])
+@app.route("/restaurant_pizzas", methods=['POST'])
 def create_restaurant_pizza():
     data = request.get_json()
     try:
@@ -57,5 +62,9 @@ def create_restaurant_pizza():
     except Exception as e:
         return jsonify({"errors": ["Invalid input"]}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def index():
+    return "<h1>Code challenge</h1>"
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
